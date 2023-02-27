6058d9334ed3374a33199290df5e77dc5a5748e0
Vishakh Abhayan <vishakhabhayan0@gmail.com>
2023-02-27 16:42:20 +0530

    Banner

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -3,7 +3,20 @@ import "./Banner.css";
 function Banner() {
   return (
     <div>
-      <div className="BannerContainer"></div>
+      <div className="backDrop">
+        <div className="content">
+          <h1>MOVIE NAME</h1>
+          <div className="banner_button">
+            <button>Play</button>
+            <button>My List</button>
+          </div>
+          <h2>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam
+            maxime corrupti, soluta numquam inventore earum.
+          </h2>
+        </div>
+        <div className="Fade"></div>
+      </div>
     </div>
   );
 }
