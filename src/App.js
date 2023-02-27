5a777c6e7ef8241b7caa5f25501c100eb750bf72
Vishakh Abhayan <vishakhabhayan0@gmail.com>
2023-02-27 22:26:00 +0530

    Row of posters

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import Header from "./Components/Header/Header";
 import "./App.css";
 import Banner from "./Components/Banner/Banner";
+import Rowpost from "./Components/rowpost/Rowpost";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Banner />
+      <Rowpost />
     </div>
   );
 }
---
+++ src/Components/rowpost/Rowpost.jsx
@@ -0,0 +1,67 @@
+import React from "react";
+import "./Rowpost.css";
+function Rowpost() {
+  return (
+    <div>
+      <div className="postersRow">
+        <h2>Most Popular</h2>
+        <div className="posters">
+          <img
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQI97-cE50f9k3OJbbFUqGC-gPZvIDMIo-baQ&usqp=CAU"
+            alt=""
+          />
+          <img
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQI97-cE50f9k3OJbbFUqGC-gPZvIDMIo-baQ&usqp=CAU"
+            alt=""
+          />
+          <img
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQI97-cE50f9k3OJbbFUqGC-gPZvIDMIo-baQ&usqp=CAU"
+            alt=""
+          />
+          <img
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQI97-cE50f9k3OJbbFUqGC-gPZvIDMIo-baQ&usqp=CAU"
+            alt=""
+          />
+          <img
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQI97-cE50f9k3OJbbFUqGC-gPZvIDMIo-baQ&usqp=CAU"
+            alt=""
+          />
+          <img
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQI97-cE50f9k3OJbbFUqGC-gPZvIDMIo-baQ&usqp=CAU"
+            alt=""
+          />
+          <img
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQI97-cE50f9k3OJbbFUqGC-gPZvIDMIo-baQ&usqp=CAU"
+            alt=""
+          />
+          <img
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQI97-cE50f9k3OJbbFUqGC-gPZvIDMIo-baQ&usqp=CAU"
+            alt=""
+          />
+          <img
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQI97-cE50f9k3OJbbFUqGC-gPZvIDMIo-baQ&usqp=CAU"
+            alt=""
+          />
+          <img
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQI97-cE50f9k3OJbbFUqGC-gPZvIDMIo-baQ&usqp=CAU"
+            alt=""
+          />
+          <img
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQI97-cE50f9k3OJbbFUqGC-gPZvIDMIo-baQ&usqp=CAU"
+            alt=""
+          />
+          <img
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQI97-cE50f9k3OJbbFUqGC-gPZvIDMIo-baQ&usqp=CAU"
+            alt=""
+          />
+          <img
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQI97-cE50f9k3OJbbFUqGC-gPZvIDMIo-baQ&usqp=CAU"
+            alt=""
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Rowpost;
