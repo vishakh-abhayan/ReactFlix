6526b61bf7e994e3e05910748cc60932523f7dc7
Vishakh Abhayan <vishakhabhayan0@gmail.com>
2023-03-16 18:29:16 +0530

    loding delay fixed

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -6,6 +6,11 @@ import "./Banner.css";
 function Banner(props) {
   const [movie, setMovie] = useState([]);
   useEffect(() => {
+    axios.get(props.url).then((response) => {
+      let x = Math.floor(Math.random() * 20);
+      console.log(response.data.results[x]);
+      setMovie(response.data.results[x]);
+    });
     setInterval(() => {
       axios.get(props.url).then((response) => {
         let x = Math.floor(Math.random() * 20);
